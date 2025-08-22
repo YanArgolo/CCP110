7e4cf1472a96060d28d0699265b253644e2f7c91
YanArgolo <yanargolodossatnos@gmail.com>
2025-08-21 22:35:12 -0300

    Exercícios 21/08/2025

--- Exercicio1.py
+++ Exercicio1.py
@@ -3,6 +3,7 @@ altura = float(input("Digite sua altura:"))
 peso = (72.7 * altura) - 58
 print(f"Seu peso ideal é: {peso: .2f}kg")
 
+#Exercício 2
 km = float(input("Digite a quantidade de quilômetros rodados:"))
 dias = int(input("Digite a quantidade de dias:"))
 total = (km * 0.15) + (dias * 60)
