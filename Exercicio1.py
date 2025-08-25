8f20889125396c2947c5e4666f2d81116d83ac06
YanArgolo <yanargolodossantos@gmail.com>
2025-08-25 16:39:27 -0300

    Exercicios 21/08

--- Exercicio1.py
+++ Exercicio1.py
@@ -1,10 +1,34 @@
 # #Exercício 1 
-altura = float(input("Digite sua altura:"))
-peso = (72.7 * altura) - 58
-print(f"Seu peso ideal é: {peso: .2f}kg")
+# altura = float(input("Digite sua altura:"))
+# peso = (72.7 * altura) - 58
+# print(f"Seu peso ideal é: {peso: .2f}kg")
 
-#Exercício 2
-km = float(input("Digite a quantidade de quilômetros rodados:"))
-dias = int(input("Digite a quantidade de dias:"))
-total = (km * 0.15) + (dias * 60)
-print(f"Total a pagar: {total: .2f}")
+# #Exercício 2
+# km = float(input("Digite a quantidade de quilômetros rodados:"))
+# dias = int(input("Digite a quantidade de dias:"))
+# total = (km * 0.15) + (dias * 60)
+# print(f"Total a pagar: {total: .2f}")
+
+# Exercicio 3
+# dias = int(input("Digite a quantidade de dias:"))
+# horas = int(input("Digite a quantidade de horas:"))
+# minutos = int(input("Digite a quantidade de minutos:"))
+# segundos = int(input("Digite a quantidade de segundos:"))
+# TempoTotal = (dias * 86400) + (horas * 3600) + (minutos * 60) + (segundos * 1)
+# print(f"O tempo total foi de: {TempoTotal: .0f}")
+
+#Exercicio 4
+# htrabalho = float(input("Digite o valor da hora de trabalho:"))
+# ntrabalho = float(input("Digite o numero de horas trabalhadas no mes:"))
+# SalarioBruto = (htrabalho * ntrabalho)
+# ir = (SalarioBruto * 11/100)
+# inss = (SalarioBruto * 8/100)
+# sindic = (SalarioBruto * 5/100)
+# SalarioLiq = (SalarioBruto) - (ir) - (inss) - (sindic)
+# print("----------------------------")
+# print(f"+ Salário Bruto: R$ {SalarioBruto: .2f}")
+# print(f"- Imposto de Renda (11%): R$ {ir: .2f}")
+# print(f"- INSS (8%): R$ {inss: .2f}")
+# print(f"- Sindicato (5%): R$ {sindic: .2f}")
+# print("----------------------------")
+# print(f"= Salário Líquido: R$ {SalarioLiq: .2f}")
