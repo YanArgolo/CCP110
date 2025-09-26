fd908e3d1bae0fc47c52c2339cca9ae4f4cc0cb8
YanArgolo <yanargolodossatnos@gmail.com>
2025-09-25 22:27:17 -0300

    lab 07

---
+++ lab07.py
@@ -0,0 +1,37 @@
+# Exercicio 1
+# x = 5
+# def f1():
+#     global x
+#     x = 4
+# def f2(a,b):
+#     global x
+#     return a+b+x
+# f1()
+# total = f2(1,2)
+# print(total)
+
+# Exercicio 2
+# x = 5
+# def f1():
+#     global x
+#     X = 4
+# def f2(a,b):
+#     X = 3
+#     return a+b+x
+# f1()
+# total = f2(1,2)
+# print(total)
+
+# Exercicio 3
+# from math import sqrt
+# def hip(lado1, lado2):
+#     hipotenusa = (lado1**2 + lado2**2)
+#     resultado = sqrt(hipotenusa)
+#     return resultado
+# lado1 = float(input("Digite o primeiro lado do triângulo: "))
+# lado2 = float(input("Digite o segundo lado do triângulo: "))
+# print(f"Hipotenusa:{hip(lado1,lado2): .2f}")
+
+# Exercicio 4
+def maximo(x, y):
+    
