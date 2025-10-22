14a849fd1cd17479ae06e087e12b5e6148ea3eed
YanArgolo <yanargolodossantos@gmail.com>
2025-10-22 17:47:42 -0300

    atualizado

--- aula08.py
+++ aula08.py
@@ -24,13 +24,28 @@
 #   Exercicio 4
 # lista = []
 # for c in range(10):
+#     n = int(input("Digite um numero: "))
+#     lista.append(n)
+# print(lista)
+
+# soma_pares = 0
+# for i in range(len(lista)):
+#     if lista[i] %2 == 0:
+#         soma_pares += lista[i]
+# print(f'A soma dos elementos pares eh: {soma_pares}')
+
+# soma_indice = 0
+# for i in range(len(lista)):
+#     if i %2 ==0:
+#         soma_indice += lista[i]
+# print(f"A soma dos indices pares eh: {soma_indice}")
 
 
 #   Exercicio 5
-lista = []
-for i in range(5):
-    n = int(input("Digite um numero: "))
-    lista.append(n)
-print(lista)
-lista_reversa = lista(i-1)
-print(lista_reversa)
+# lista = []
+# for i in range(5):
+#     n = int(input("Digite um numero: "))
+#     lista.append(n)
+# print(lista)
+# lista_reversa = lista[::-1] #:: serve para omitir o início e fim da lista
+# print(lista_reversa)
