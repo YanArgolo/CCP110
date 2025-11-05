d72dea25d0ec6cb358f59dc60ba831af56da6812
YanArgolo <yanargolodossantos@gmail.com>
2025-11-04 22:15:21 -0300

    04/11

---
+++ aula13/Módulos/exemplo1.py
@@ -0,0 +1,8 @@
+
+def main():
+    print("Arquivo calculo.py carregado")
+
+def media():
+    return float(sum(lista)) / len(lista)
+
+if _name_=="    "
---
+++ aula13/exercicio3.py
@@ -0,0 +1,24 @@
+
+
+with open("arquivo.txt" , "p" ,encoding="utf=8") as arquivo:
+    conteudo = arquivo.read()
+    
+print(conteudo)
+texto_sem_pontuacao = ""
+for char in conteudo:
+    if char.isalnum() or char.isspace():
+        texto_sem_pontuacao += char
+
+print(texto_sem_pontuacao)
+
+lista_palavras = texto_sem_pontuacao.split()
+print(lista_palavras)
+
+maiores_palavras = []
+maior_tamanho = 0
+for palavra in lista_palavras:
+    if len(palavra) > maior_tamanho:
+        maior_tamanho = len(palavra)
+        maiores_palavras = [palavra]
+    elif len(palavra) == maior_tamanho:
+        maiores_palavras.append(palavra)
